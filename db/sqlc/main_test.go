c1ac97d91419e67c646085895710d73a818f31f6
cuzz <cuzz1234@163.com>
2020-12-08 20:39:43 +0800

    api and config

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -2,12 +2,12 @@ package db
 
 import (
 	"database/sql"
+	"github.com/cuzz1/simplebank/util"
+	_ "github.com/lib/pq"
 	"log"
 	"os"
 	"testing"
-	_ "github.com/lib/pq"
 )
-
 
 const (
 	dbDriver = "postgres"
@@ -17,9 +17,13 @@ const (
 var testQueries *Queries
 var testDB *sql.DB
 
-func TestMain(m *testing.M)  {
-	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+func TestMain(m *testing.M) {
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
--- go.mod
+++ go.mod
@@ -3,6 +3,16 @@ module github.com/cuzz1/simplebank
 go 1.13
 
 require (
+	github.com/gin-gonic/gin v1.6.3
+	github.com/go-playground/validator/v10 v10.4.1 // indirect
+	github.com/golang/protobuf v1.4.3 // indirect
+	github.com/json-iterator/go v1.1.10 // indirect
 	github.com/lib/pq v1.9.0
+	github.com/spf13/viper v1.7.1
 	github.com/stretchr/testify v1.6.1
+	github.com/ugorji/go v1.2.1 // indirect
+	golang.org/x/crypto v0.0.0-20201203163018-be400aefbc4c // indirect
+	golang.org/x/sys v0.0.0-20201207223542-d4d67f95c62d // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
